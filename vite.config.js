86efcc0a29ad650d0f3f2c3e4b5f7d18247238ac
KanyalBhupendra9634 <kanyalgboby@gmail.com>
2025-05-18 16:21:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    fs: {
+      allow: ['.', './src', './public']
+    }
+  },
+  resolve: {
+    alias: {
+      '/js': '/src/js',
+      '/css': '/public/css'
+    }
+  }
 })
